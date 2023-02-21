78f1ddfbaf7d17bdfe7b60278cc746143171c43c
haliltuzcu <007canberk@gmail.com>
2023-02-21 22:07:12 +0300

    cart eklendi

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,13 +1,17 @@
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import { data } from "../helpers/data";
 import PlayerCard from "./PlayerCard";
 
 const CardContainer = () => {
   return (
-    <Container className="card-container">
-      {data.map((player) => (
+    <Container className="card-container rounded-4 my-4 p-3">
+        <Row className="g-3 justify-content-center">
+      {data.map((player,i) => (
+        <Col md={6} lg={4} xl={3} key={i}  >
         <PlayerCard {...player} />
+        </Col>
       ))}
+      </Row>
     </Container>
   );
 };
--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -2,8 +2,8 @@ import Card from "react-bootstrap/Card";
 
 const PlayerCard = ({name, img, statistics}) => {
   return (
-    <Card>
-      <Card.Img variant="top" src={img} />
+    <Card className="rounded-2 m-auto player-card "role="button">
+      <Card.Img className="player-logo" variant="top" src={img} />
       <Card.Footer>
         <Card.Title>{name}</Card.Title>
       </Card.Footer>
