973ddd0282df9c87765f47832495c52af6862522
haliltuzcu <007canberk@gmail.com>
2023-02-21 22:58:29 +0300

    input eklendi

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,18 +1,31 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { useState } from "react";
+import { Col, Container, Row, Form } from "react-bootstrap";
 import { data } from "../helpers/data";
 import PlayerCard from "./PlayerCard";
 
 const CardContainer = () => {
+  const [search, setSearch] = useState("");
   return (
-    <Container className="card-container rounded-4 my-4 p-3">
+    <>
+      <Form.Control
+        placeholder="Sherch Player..."
+        className="w-50 m-auto"
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <Container className="card-container rounded-4 my-4 p-3">
         <Row className="g-3 justify-content-center">
-      {data.map((player,i) => (
-        <Col md={6} lg={4} xl={3} key={i}  >
-        <PlayerCard {...player} />
-        </Col>
-      ))}
-      </Row>
-    </Container>
+          {data
+            .filter((player) =>
+              player.name.toLowerCase().includes(search.trim().toLowerCase())
+            )
+            .map((player, i) => (
+              <Col md={6} lg={4} xl={3} key={i}>
+                <PlayerCard {...player} />
+              </Col>
+            ))}
+        </Row>
+      </Container>
+    </>
   );
 };
 
